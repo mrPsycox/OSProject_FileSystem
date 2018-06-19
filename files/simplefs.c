abe7d5f93514e0d1b9f6f6eb6b609551659a71c5
Simone <silvestri.1699926@studenti.uniroma1.it>
2018-06-19 16:47:12 +0200

    restarting the project: finishing the simplefs_CreateFile

--- files/simplefs.c
+++ files/simplefs.c
@@ -20,8 +20,8 @@ DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk){
 	FirstDirectoryBlock* fdb = malloc(sizeof(FirstDirectoryBlock));
 	int ret;
 	ret = DiskDriver_readBlock(disk, fdb, 0); //leggo il primo blocco
-	if(ret == -1){  //ciò sta a signicare che il blocco è libero
-		free(fdb);		//libero il firstdirectoryblock, ritorno NULL
+	if(ret == -1){  //ciò sta a signicare che il blocco è libero, perciò non ho piu bisogno del fdb
+		free(fdb);		
 		return NULL;
 	}
 	DirectoryHandle* dh = malloc(sizeof(DirectoryHandle*));
@@ -62,7 +62,7 @@ void SimpleFS_format(SimpleFS* fs){
 	fs->disk->header->free_blocks = fs->disk->header->num_blocks;			//logicamente i blocchi liberi devono essere uguali ai blocchi presenti
 	
 	int size_bitmap = fs->disk->header->bitmap_entries;
-	//ora metto a 0 tutto quando i dati del disco
+	//orainizializzo il mettendo a '\0' tutti quanti i dati del disco, 
 	memset(fs->disk->bitmap_data,'\0',size_bitmap);		//metto a '\0' == equivalente di char == 0
 	
 	int ret;
@@ -79,20 +79,20 @@ void SimpleFS_format(SimpleFS* fs){
 // an empty file consists only of a block of type FirstBlock
 FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
 	//mi prendo dall'input le struct necessarie
-	
+	 
 	FirstDirectoryBlock* fdb = d->dcb;
 	DiskDriver* disk = d->sfs->disk;
 	
 	//mi calcolo il massimo spazio necessario per le due struct:
 		//-First Directory Block
 		//-Directory Block
-		
+	//calcolo questo spazio_max controllando nel .h le allocazioni di struct necessarie da fare
 	int max_req_space_fdb = (BLOCK_SIZE - sizeof(BlockHeader) - sizeof(FileControlBlock) - sizeof(int))/sizeof(int);
 	int max_req_space_db = (BLOCK_SIZE - sizeof(BlockHeader) - sizeof(int))/sizeof(int); 
 	
-	//ora devo controllare che il file non esista già il file
+	//PRIMO STEP: ora devo controllare che il file non esista già
 	//cominciamo a controllare nel FirstDirectoryBlock
-	if(fdb->num_entries > 0){		//se le num entries sono < 0 logicamente salta queste istruzioni
+	if(fdb->num_entries > 0){		//se le num entries sono < 0 non vado nemmeno a cercare
 		
 		FirstFileBlock ffb;	//lo userò per fare il confronto nella read block, lo faccio scorrere controllando tutti gli elementi
 							//di file_blocks[i]
@@ -137,6 +137,117 @@ FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
 		printf("SimpleFS_createFile: cannot have a new free block from the disk :(\n");
 		return NULL;
 	}
-	FirstFileBlock* newfile_created = calloc(1,sizeof(FirstFileBlock));
+	FirstFileBlock* newfile_created = calloc(1,sizeof(FirstFileBlock));   //uso la calloc per preparare l'allocazione
+	//già pronta per l'uso, senza l'utilizzo di una memset successiva
+	//popolo la struct
+	newfile_created->header.block_in_file = 0;	//setto ai valori di default
+	newfile_created->header.next_block = -1;
+	newfile_created->header.previous_block = -1;
+	
+	strcpy(newfile_created->fcb.name,filename);  // copio il nome del filename
+	newfile_created->fcb.block_in_disk = new_freeblock;  //	assegno il blocco libero ottenuto in precedenza
+	newfile_created->fcb.directory_block = fdb->fcb.block_in_disk;   //assegno al nuovo file il blocco nel disco nel FileControlBlock ottenuto da input
+	newfile_created->fcb.is_dir = 0; //0 file, 1 directory
+	
+	//ora provo a scrivere il blocco sul disco
+	int ret;
+	ret = DiskDriver_writeBlock(disk,newfile_created, new_freeblock);
+	if(ret == -1){
+		printf("SimpleFS_createFile: cannot write the new free block on disk");
+		return NULL;
+	}
+	
+	
+	
+	
+	
+	//ora comincio a controllare la presenza di blocchi liberi sia nel first directoryblock che nel directory block
+	int block_found = 0; //0 in caso negativo, 1 in caso positivo cioe spazio libero trovato
+	int block_entry = 0; //mi salvo la entry del blocco in modo da tracciarne la posizione
+	int not_spacein_firstdirectoryblock = 0; //0 se sta nel firstdirectoryblock, 1 directory block
+	int block_number = fdb->fcb.block_in_disk;
+	int blockinFile = 0; //numero delle iterazioni?
+	int file_saved_on_fdb_or_db = 0; // 0 se il file viene salvato nel first directory block,
+									// 1 altrimenti
+	
+	
+	DirectoryBlock last_db; //la utilizzo come appoggio
+	
+	if(fdb->num_entries < max_req_space_fdb){
+			int* blocks = fdb->file_blocks;
+			for(int j = 0; j < max_req_space_fdb; j++){
+				if(blocks[j] == 0){  //significa che tra i blocchi ce ne è uno libero
+					block_found = 1;
+					block_entry = j;
+					break;
+				}
+			}
+	}else{  //in caso che lo spazio non è presente nel firstdirectory, provo nel directory blocks
+			not_spacein_firstdirectoryblock = 1;   //mi salvo questa informazione
+			int next_block = fdb->header.next_block;
+			
+			while(next_block != -1 && !block_found){ //finche ho un nuovo blocco libero e finche non ho trovato uno spazio
+				ret = DiskDriver_readBlock(disk,&last_db,next_block);
+				if(ret == -1){
+					printf("SimpleFS_createFile: cannot read a block searching in DirectoryBlock");
+					DiskDriver_freeBlock(disk, fdb->fcb.block_in_disk);
+					return NULL;
+				}
+				int* blocks = fdb->file_blocks;		//cerco nello stesso modo di prima
+				block_number = next_block;
+				blockinFile++;
+				for(int i = 0; i < max_free_space_db;i++){
+					if(blocks[i] == 0){
+						found = 1;
+						entry = i;
+						break;
+					}
+				}
+				file_saved_on_fdb_or_db = 1;
+				next_block = last_db.header.next_block;
+			} 
+		}
+		if(!found){ //in caso non ci sia piu spazio
+			//devo allocare un un nuovo directory block e popolare il suo header
+			DirectoryBlock new_db = {0};
+			new_db.header.next_block = -1;
+			new_db.header.block_in_file = blockinFile;
+			new_db.header.previous_block = block_number;
+			new_db.file_blocks[0] = new_freeblock;
+			
+			int new_dir_block = DiskDriver_freeBlock(disk,fdb->header.first_free_block);
+			if(new_freeblock == -1){
+				printf("SimpleFS_CreateFile: impossibile to create file");
+				DiskDriver_freeBlock(disk,fdb->fcb.block_in_disk);
+				return NULL;
+			}
+			
+		}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
